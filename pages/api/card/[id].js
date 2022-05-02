261887e57d045a116a938e1713c53a12dac8c38d
Hisham Almannai <hisham@gmx.de>
2022-05-02 10:06:49 +0200

    Connect to Atlas

--- pages/api/card/[id].js
+++ pages/api/card/[id].js
@@ -1,4 +1,5 @@
 import Card from "../../../src/models/Card";
+import User from "../../../src/models/User";
 
 export default async function handler(request, response) {
   const { id } = request.query;
@@ -10,8 +11,20 @@ export default async function handler(request, response) {
     response.status(200).json({ message: "card deleted", card: deletedCard });
   } else if (request.method === "PUT") {
     const data = JSON.parse(request.body);
-    const changedCard = await Card.findByIdAndUpdate(id, data, { new: true });
-    response.status(200).json({ message: "card updated", card: changedCard });
+    const card = await Card.findById(id);
+    const changedCard = await Card.findByIdAndUpdate(
+      id,
+      { content: data.content },
+      { new: true }
+    );
+    const changedUser = await User.findByIdAndUpdate(
+      card.user,
+      { content: data.name },
+      { new: true }
+    );
+    response
+      .status(200)
+      .json({ message: "card updated", card: changedCard, changedUser });
   } else {
     const singleCard = await Card.findById(id);
     response.status(200).json(singleCard);
